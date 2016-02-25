6d75f94d5683610be005aa698cc6d1f2ee03f4aa
Geralastel <gian.m.colucci@gmail.com>
2016-02-24 19:38:36 -0500

    Initial Commit

---
+++ A1_COMMON/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  A1_COMMON
+//
+//  Created by Gian Matteo Colucci on 2016-02-24.
+//  Copyright © 2016 GIan Matteo Colucci. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
